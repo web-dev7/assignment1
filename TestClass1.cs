4363dea8153ab3a1fc8255e8a4cba770258fb60c
vignesh <ssaivignesh7@gmail.com>
2023-01-04 22:09:26 +0530

    Add project files.

---
+++ TestClass1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment
+{
+    public class TestClass1
+    {
+         public int findLength(string str)
+        {
+            try {
+                int length = str.Length;
+                return length;
+            }
+            catch(ArgumentNullException e)
+            {
+                throw e;
+            }
+           
+        }
+    }
+}
